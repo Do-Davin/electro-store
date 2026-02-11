d3bdddd7f54aa849ff73900cad6d2e8c962d1950
Do Davin <dodavin96@gmail.com>
2026-02-11 19:46:15 +0700

    (fix): migrations DB

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -7,6 +7,5 @@ export default new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  migrations: [__dirname + '/migrations/*{.ts,.js}'],
-  synchronize: false,
+  synchronize: true,
 });
